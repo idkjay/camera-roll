e9671e3fe0ae103c6a927695673b59f99c3e16b4
Jay <jordanchu95@gmail.com>
2020-10-21 01:43:34 -0400

    com

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/App.css";
 import Gallery from 'react-grid-gallery';
-import { render } from 'react-dom';
-
-
 
 const Picture = ({results}) => {
     return(
-
         <Gallery
             images={results.urls.raw}
         />
